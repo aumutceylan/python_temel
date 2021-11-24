8fbbd3c436e4667d166caab7a2b83c063c14d289
ali <aumutceylan@gmail.com>
2021-11-24 15:23:15 +0300

    ilkcommit

---
+++ calısma.py
@@ -0,0 +1,26 @@
+# initializing the data and an empty list
+data = [[4, "Ali", 7], ["Ceylan", 8], [9, 1, "Themanhattan", 2, "Gece"]]
+flat_list = []
+
+# iterating over the data
+for item in data:
+    # appending elements to the flat_list
+    flat_list += item
+
+# printing the resultantn flat_list
+print(flat_list)
+
+###############################################################
+
+# create a list of prime numbers
+prime_numbers = [2, 9, 7, 4, 1]
+
+# reverse the order of list elements
+prime_numbers.reverse()
+
+
+print('Reversed List:', prime_numbers)
+
+# Output: Reversed List: [2, 9, 7, 4, 1]
+
+#########################################################
